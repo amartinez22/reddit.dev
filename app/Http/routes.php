d2690d1fe45bf3f4673291bce95809f5c403bf9e
Analyssa Martinez <analyssamartinez@Analyssas-MacBook-Pro.local>
2017-08-23 14:53:00 -0500

    Added first route file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,63 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function () {
+// 	$result = 1 + 1;
+// 	echo $result;
+// });
+
+// Route::get('/sayhello', function () {
+// 	return "Hello world!";
+// }
+
+
+// Route::get('/sayhello/{name}', function($name) {
+// 	return "Hello, $name!";
+// });
+
+// Route::get('/sayhello/{name?}', function($name = "World") {
+// 	if($name == "Chris"){
+// 	return redirect('/');	
+// 	} else {
+// 	return "Hello, $name";
+// 	}
+// });
+
+// // Route with required parameters
+// Route::get('/sayhello/{firstName}/{lastname}', function($firstName, $lastName) {
+// 	return "Hi there, $firstName $lastName";
+// });
+
+
+// Route::post('/save', function() {
+
+// });
+
+Route::get('/uppercase/{word}', function($word) {
+	return strtoupper($word);
+
+});
+
+
+Route::get('/increment/{num}', function($num) {
+	if(is_numeric($number)){
+	return $num + 1;
+	} else {
+		return 1;
+	}
+});
+
+
+Route::get('add/{num1}{num2}', function($num1, $num2) {
+	if(is_numeric($num1) && is_numeric($num2)) {
+		return $num1 + $num2;
+	}
+});
+
+
+
+
+
+
+
